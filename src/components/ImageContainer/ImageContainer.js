54f76ab22320b1a603b6b62e8b10ab3b1db5533f
Carlos Gonzalez <carlossgv@gmail.com>
2021-04-18 19:14:19 -0400

    ImageContainer route implemented

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -3,8 +3,8 @@ import FinishContainer from '../FinishContainer/FinishContainer';
 import OptionsSquare from '../OptionsSquare/OptionsSquare';
 import './ImageContainer.css';
 import StopWatch from '../Stopwatch/Stopwatch';
-import { useLocation } from 'react-router-dom'
-
+import { useLocation } from 'react-router-dom';
+import { Image } from '../../Functions';
 
 const ImageContainer = () => {
   const [coordinates, setCoordinates] = useState({
@@ -14,34 +14,33 @@ const ImageContainer = () => {
     optionsY: '',
   });
   const [visibility, setVisibility] = useState('hidden');
-  // const [containerOptions, setContainerOptions] = useState(options);
   const [isFinished, setIsFinished] = useState(null);
   const [time, setTime] = useState(0);
   const [options, setOptions] = useState(null);
   const [image, setImage] = useState({
     url: '',
     Image: '',
+    name: HeaderView()
   });
   console.log(options);
 
-  
   function HeaderView() {
     const location = useLocation();
-    return location.pathname.split('/')[2]
+    return location.pathname.split('/')[2];
   }
 
-  const imageName = HeaderView();
+  // const imageName = 
 
   useEffect(() => {
     console.log('rebooting options list');
-    const imageObject = Image('showsImage');
+    const imageObject = Image(image.name);
 
-    import(`../../static/images/${imageName}.jpg`).then((imageFile) => {
+    import(`../../static/images/${image.name}.jpg`).then((imageFile) => {
       setImage({ url: imageFile.default, Image: imageObject });
     });
 
     async function createOptionsArray(imageName) {
-      const imageOptions = await imageObject.getImageData(imageName);
+      const imageOptions = await imageObject.getImageData(image.name);
 
       const asyncOptions = [];
 
@@ -52,7 +51,7 @@ const ImageContainer = () => {
       setOptions(asyncOptions);
     }
 
-    createOptionsArray(imageName);
+    createOptionsArray(image.name);
   }, []);
 
   const handleOnMouseDown = (e) => {
@@ -60,7 +59,7 @@ const ImageContainer = () => {
       const img = document.querySelector('#nonModalImage');
       setVisibility('hidden');
 
-      const returnedCoordinates = image.getCoordinates(e, img);
+      const returnedCoordinates = image.Image.getCoordinates(e, img);
 
       setCoordinates({
         posX: returnedCoordinates.posX,
@@ -90,7 +89,7 @@ const ImageContainer = () => {
 
     const selection = e.target.innerHTML;
 
-    const newOptionsObject = image.checkClick(
+    const newOptionsObject = image.Image.checkClick(
       coordinates.posX,
       coordinates.posY,
       selection,
@@ -108,7 +107,7 @@ const ImageContainer = () => {
     console.log(newOptionsArray);
 
     //TODO: CHECK IF ALL STUFF IS LOCATED
-    const isFinished = image.checkIsFinished(newOptionsArray);
+    const isFinished = image.Image.checkIsFinished(newOptionsArray);
     console.log(isFinished);
     if (isFinished) {
       setVisibility('hidden');
@@ -123,16 +122,20 @@ const ImageContainer = () => {
 
   return (
     <div className="imageContainer" onMouseDown={handleOnMouseDown}>
-      {isFinished && <FinishContainer display={'block'} imageName={image.name} time={time} />}
+      {isFinished && (
+        <FinishContainer display={'block'} imageName={image.name} time={time} />
+      )}
       <StopWatch isFinished={isFinished} logTime={logTime} />
       <div className="image">
         <img id="nonModalImage" src={image.url} alt={image.Image.alt} />
-        <OptionsSquare
-          visibility={visibility}
-          coordinates={coordinates}
-          options={options}
-          handleOptionsClick={handleOptionsClick}
-        />
+        {options && (
+          <OptionsSquare
+            visibility={visibility}
+            coordinates={coordinates}
+            options={options}
+            handleOptionsClick={handleOptionsClick}
+          />
+        )}
       </div>
       <div id="myModal" className="modal">
         <img
